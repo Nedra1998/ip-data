fe1808bea5cf3a2c93e1d60c994a7675627c42d4
Arden Rasmussen <ardenisthebest@gmail.com>
2019-02-09 18:58:32 -0800

    Changed script

--- save_ip.sh
+++ save_ip.sh
@@ -2,6 +2,8 @@
 
 ip a > ssh_connection.txt
 
+git config --global user.name "Arden Rasmussen"
+git config --global user.email "ardenisthebest@gmail.com"
 git add -A
 git commit -m"IP ADDRESS"
 git push
